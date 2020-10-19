092bc7c12bb47e42585c61a5c0345b6950d748ff
the78mole <me@the78mole.de>
2020-10-19 15:10:54 +0200

    Initial commit after @iobroker/adapter-create

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+	"rentman adapter settings": {
+		"en": "Adapter settings for rentman",
+		"de": "Adaptereinstellungen für rentman",
+		"ru": "Настройки адаптера для rentman",
+		"pt": "Configurações do adaptador para rentman",
+		"nl": "Adapterinstellingen voor rentman",
+		"fr": "Paramètres d'adaptateur pour rentman",
+		"it": "Impostazioni dell'adattatore per rentman",
+		"es": "Ajustes del adaptador para rentman",
+		"pl": "Ustawienia adaptera dla rentman",
+		"zh-cn": "rentman的适配器设置"
+	},
+	"option1": {
+		"en": "option1",
+		"de": "Option 1",
+		"ru": "Опция 1",
+		"pt": "Opção 1",
+		"nl": "Optie 1",
+		"fr": "Option 1",
+		"it": "opzione 1",
+		"es": "Opción 1",
+		"pl": "opcja 1",
+		"zh-cn": "选项1"
+	},
+	"option2": {
+		"en": "option2",
+		"de": "Option 2",
+		"ru": "option2",
+		"pt": "opção 2",
+		"nl": "Optie 2",
+		"fr": "Option 2",
+		"it": "opzione 2",
+		"es": "opcion 2",
+		"pl": "Opcja 2",
+		"zh-cn": "选项2"
+	}
+};
